6d6b0eafdb0bd3360a8bf3e6ad429c79774c97e9
Renjie Zhu <renjie.j.zhu@gmail.com>
2018-12-06 02:59:03 -0800

    clean up csv files

---
+++ code.py
@@ -0,0 +1,21 @@
+#
+# ECE 225A Project: Air Quality
+#
+# Nov 27, 2018 by Renjie Zhu
+#
+
+import pandas as pd
+
+bj_12 = pd.read_csv("data/Beijing_2012.csv", encoding="ISO-8859-1")
+bj_13 = pd.read_csv("data/Beijing_2013.csv", encoding="ISO-8859-1")
+bj_14 = pd.read_csv("data/Beijing_2014.csv", encoding="ISO-8859-1")
+bj_15 = pd.read_csv("data/Beijing_2015.csv", encoding="ISO-8859-1")
+bj_16 = pd.read_csv("data/Beijing_2016.csv", encoding="ISO-8859-1")
+bj_17 = pd.read_csv("data/Beijing_2017.csv", encoding="ISO-8859-1")
+
+sh_12 = pd.read_csv("data/Shanghai_2012.csv", encoding="ISO-8859-1")
+sh_13 = pd.read_csv("data/Shanghai_2013.csv", encoding="ISO-8859-1")
+sh_14 = pd.read_csv("data/Shanghai_2014.csv", encoding="ISO-8859-1")
+sh_15 = pd.read_csv("data/Shanghai_2015.csv", encoding="ISO-8859-1")
+sh_16 = pd.read_csv("data/Shanghai_2016.csv", encoding="ISO-8859-1")
+sh_17 = pd.read_csv("data/Shanghai_2017.csv", encoding="ISO-8859-1")
